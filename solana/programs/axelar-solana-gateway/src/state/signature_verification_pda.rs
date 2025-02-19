900ed39b288c242065949e18f9770fa4074ab93d
Foivos <foivos@umich.edu>
2025-02-19 18:22:39 +0200

    moved SignatureVerificationSessionData population into it's own module.

--- solana/programs/axelar-solana-gateway/src/state/signature_verification_pda.rs
+++ solana/programs/axelar-solana-gateway/src/state/signature_verification_pda.rs
@@ -1,7 +1,11 @@
 //! Module for the signature verification session PDA data layout type.
 
 use bytemuck::{Pod, Zeroable};
-use program_utils::BytemuckedPda;
+use program_utils::{BytemuckedPda, ValidPDA};
+use solana_program::{account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey};
+use core::mem::size_of;
+
+use crate::{error::GatewayError, seed_prefixes};
 
 use super::signature_verification::SignatureVerification;
 
@@ -23,6 +27,59 @@ pub struct SignatureVerificationSessionData {
 }
 
 impl BytemuckedPda for SignatureVerificationSessionData {}
+
+impl SignatureVerificationSessionData {
+    pub(crate) fn populate<'a>(
+        pda: &AccountInfo<'a>, 
+        gateway_root_pda: &AccountInfo<'a>, 
+        payer: &AccountInfo<'a>, 
+        system_program: &AccountInfo<'a>, 
+        program_id: &Pubkey, 
+        merkle_root:[u8; 32]
+    )-> Result<(), ProgramError>{
+        // Check: Verification PDA can be derived from provided seeds.
+        // using canonical bump for the session account
+        let (verification_session_pda, bump) =
+            crate::get_signature_verification_pda(gateway_root_pda.key, &merkle_root);
+        if verification_session_pda != *pda.key {
+            return Err(GatewayError::InvalidVerificationSessionPDA.into());
+        }
+
+        // Check: the verification session account has not been initialised already
+        pda
+            .check_uninitialized_pda()
+            .map_err(|_err| GatewayError::VerificationSessionPDAInitialised)?;
+
+        // Use the same seeds as `[crate::get_signature_verification_pda]`, plus the
+        // bump seed.
+        let signers_seeds = &[
+            seed_prefixes::SIGNATURE_VERIFICATION_SEED,
+            gateway_root_pda.key.as_ref(),
+            &merkle_root,
+            &[bump],
+        ];
+
+        // Prepare the `create_account` instruction
+        program_utils::init_pda_raw(
+            payer,
+            pda,
+            program_id,
+            system_program,
+            size_of::<SignatureVerificationSessionData>()
+                .try_into()
+                .map_err(|_err| {
+                    solana_program::msg!("Unexpected u64 overflow in struct size");
+                    ProgramError::ArithmeticOverflow
+                })?,
+            signers_seeds,
+        )?;
+        let mut data = pda.try_borrow_mut_data()?;
+        let session = SignatureVerificationSessionData::read_mut(&mut data)
+            .ok_or(GatewayError::BytemuckDataLenInvalid)?;
+        session.bump = bump;
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
